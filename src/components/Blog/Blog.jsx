71ffab394ef9ef8c422c7c2795d61c360eba4c75
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-10-22 08:28:56 +0600

    Display Blog data and hashtags

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -2,10 +2,28 @@ import PropTypes from 'prop-types'; // ES6
 
 
 const Blog = ({blog}) => {
-    console.log(blog);
+    const {title,cover, author, author_img,posted_date, reading_time,hashtags} = blog;
     return (
         <div>
-            
+            <img src={cover} alt={`Cover picture of the title ${title}`} />
+            <div className='flex justify-between'>
+                <div className='flex'>
+                    <img className='w-14' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                </div>
+            </div>
+            <h2 className='text-4xl'>{title}</h2>
+            <p>
+                {
+                    hashtags.map(hash => <span><a href=''>#{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
